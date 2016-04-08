4b589c7bc59c69aab83b79203bf25d49efa9d7dd
Mobilszoftver laboratórium <mobsoft@aut.bme.hu>
2016-04-08 10:47:47 +0200

    Added Dagger

---
+++ app/src/main/java/aut/bme/hu/service/LoginServiceImpl.java
@@ -0,0 +1,20 @@
+package aut.bme.hu.service;
+
+import android.util.Log;
+
+import javax.inject.Singleton;
+
+import aut.bme.hu.module.SocialModule;
+import dagger.Component;
+
+/**
+ * Created by mobsoft on 2016. 04. 08..
+ */
+
+public class LoginServiceImpl implements LoginService{
+
+    @Override
+    public void login(String email, String password) {
+        Log.i("LoginServiceImpl", email);
+    }
+}
